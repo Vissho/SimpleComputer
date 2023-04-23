5a87b3071b1c3b409d7fe9d8e69c402584d40b92
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2023-04-23 16:06:08 +0700

    Added test

--- include/mySignal.h
+++ include/mySignal.h
@@ -5,6 +5,7 @@ extern int instruction_counter;
 void stop_timer (void);
 void signalhandler_1 (__attribute__ ((unused)) int signo);
 void signalhandler_2 (__attribute__ ((unused)) int signo);
+void signalhandler_3 (__attribute__ ((unused)) int signo);
 int timer (void);
 int source (void);
 int reboot (void);
